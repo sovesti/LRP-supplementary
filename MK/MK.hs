5530ee3c96471afe125e2e9633c57110c0bfcdd9
Vasily Fedorov <vasily.fedorov@arsysop.ru>
2025-12-29 01:23:22 +0300

    miniKanren + Prolog

--- MK/MK.hs
+++ MK/MK.hs
@@ -1,10 +1,12 @@
 -- Supplementary materials for the course of logic and relational programming, 2025
 -- (C) Dmitry Boulytchev, dboulytchev@gmail.com
 -- MicroKanren.
+{-# LANGUAGE DeriveFunctor #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module MK where
 
-import Data.Maybe
 import Control.Monad
 import Control.Applicative
 import qualified Term as T
@@ -15,6 +17,7 @@ data Stream a =
   Mature a (Stream a) |
   Immature (Stream a) deriving Functor
 
+pick :: Integer -> Stream a -> [a]
 pick n s =
   case n of
     0 -> []
@@ -24,43 +27,49 @@ pick n s =
            Immature fs   -> pick n fs  
 
 instance Applicative Stream where
-  pure     = undefined
-  _ <*> _  = undefined
+  pure f = Mature f $ pure f 
+  (<*>)  = ap
 
 instance Alternative Stream where
-  empty   = undefined
-  _ <|> _ = undefined
-  
+  empty   = Nil
+  Nil <|> s = s
+  Immature s <|> s' = Immature (s' <|> s)
+  Mature h tl <|> s' = Mature h $ tl <|> s' 
+    
 instance Monad Stream where
-  _ >>= _ = undefined
+  Nil >>= _ = Nil
+  Immature s >>= f = Immature $ s >>= f
+  Mature h tl >>= f = f h <|> (tl >>= f) 
 
 instance MonadPlus Stream where
-  mzero = undefined
-  mplus = undefined
+  mzero = empty
+  mplus = (<|>)
   
 type State = (T.Subst, Int)
 type Goal  = State -> Stream State
 
 infix 4 ===
 
+toStream fresh subst = Mature (subst, fresh) Nil
+
 (===) :: T.T -> T.T -> Goal
-t1 === t2 = undefined
+t1 === t2 = \(subst, fresh) -> maybe Nil (toStream fresh) (U.unify (Just subst) t1 t2)
 
 infixr 3 &&&
 
 (&&&) :: Goal -> Goal -> Goal
-g1 &&& g2 = undefined
+(&&&) = (>=>)
 
 infixr 2 |||
 
 (|||) :: Goal -> Goal -> Goal
-g1 ||| g2 = undefined
+g1 ||| g2 = \st -> g1 st <|> g2 st
 
 call_fresh :: (T.T -> Goal) -> Goal
-call_fresh f = undefined
+call_fresh f (subst, fresh) = f (T.V fresh) (subst, fresh + 1)
 
 delay :: Goal -> Goal
-delay f s = undefined
+delay f s = Immature $ f s
 
 --- Initial state & run
 initial = (T.empty, 1000)
@@ -92,5 +101,89 @@ add x y z = delay $
 s0 = run (peep [x])    (add (s o) (s o) x)
 s1 = run (peep [x])    (add x (s o) (s (s o)))
 s2 = run (peep [x, y]) (add x y (s (s o)))
-       
+  
+nil = T.C 2 []
+cons t l = T.C 3 [t, l]
 
+leo x y = delay $
+  x === o |||
+  call_fresh (\ x' ->
+  call_fresh (\ y' ->
+    x === s x' &&&
+    y === s y' &&&
+    leo x' y'
+  ))
+
+appendo l t u = delay $
+  l === nil &&& t === u |||
+  call_fresh (\ x ->
+  call_fresh (\ l' ->
+  call_fresh (\ u' ->
+    l === cons x l' &&&
+    u === cons x u' &&&
+    appendo l' t u'
+  )))  
+
+mino x l = delay $
+  l === nil |||
+  call_fresh (\ y ->
+  call_fresh (\ l' ->
+    l === cons y l' &&&
+    leo x y &&&
+    mino x l'
+  ))
+
+sorto l t = delay $
+  l === nil &&& t === nil |||
+  call_fresh (\ x ->
+  call_fresh (\ t' ->
+  call_fresh (\ l' ->
+  call_fresh (\ u ->
+  call_fresh (\ v ->
+    t === cons x t' &&&
+    appendo u (cons x v) l &&&
+    appendo u v l' &&&
+    mino x l' &&& -- like buggy sort from SLD but works
+    sorto l' t'
+  )))))
+
+fromNat :: Integer -> T.T
+fromNat 0 = o
+fromNat n = s (fromNat $ n - 1)
+
+toNat :: T.T -> Integer
+toNat (T.C 0 []) = 0
+toNat (T.C 1 [t]) = 1 + toNat t
+toNat _ = undefined
+
+showPeano :: T.T -> String
+showPeano n = show $ toNat n
+
+toPList :: [T.T] -> T.T
+toPList = foldr cons nil
+
+toHList (T.C 2 []) = []
+toHList (T.C 3 [t, l]) = t : toHList l
+toHList _ = undefined
+
+showPList l = show $ map toNat (toHList l)
+
+l0 = map showPList $ concat $
+      run
+        (peep [x])
+        (sorto (toPList [fromNat 3, fromNat 1, fromNat 5]) x)
+
+mul x y z = delay $
+  x === o &&& z === o |||
+  call_fresh (\ t ->
+  call_fresh (\ x' ->
+    x === s x' &&&
+    mul y x' t &&&
+    add t y z
+  ))
+
+-- Diverges but finds all solutions
+s4 = map (map showPeano) $
+      run
+        (peep [x, y])
+        (mul x y (fromNat 10))
--- SLD/SLD.hs
+++ SLD/SLD.hs
@@ -1,6 +1,11 @@
 -- Supplementary materials for the course of logic and relational programming, 2025
 -- (C) Dmitry Boulytchev, dboulytchev@gmail.com
 -- SLD-resolution.
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module SLD where
 
@@ -9,7 +14,7 @@ import Unify
 import qualified Data.Map as Map
 import qualified Control.Monad as Monad
 import qualified Control.Monad.State as St
-import Debug.Trace
+import Debug.Trace()
 
 -- Predicate names
 type Pred = Int
@@ -31,17 +36,23 @@ type P = [H]
 
 -- Unification for atomic formulas
 instance Unifiable A where
-  unify = undefined
+  unify Nothing _ _ = Nothing
+  unify s (p1, ts1) (p2, ts2) = if p1 /= p2 then Nothing else
+                                             let unifyPair s (t1, t2) = unify s t1 t2 in
+                                                      foldl unifyPair s (zip ts1 ts2)   
   
 -- Substitution application to atomic formulas
 instance Substitutable A where
-  apply = undefined
+  apply s (p, ts) = (p, map (apply s) ts)
+
+instance Substitutable [A] where
+  apply s = map (apply s)
 
 -- State
 --   1. A triple
 --      1. a tail of a program to match against
---	2. current goal
---	3. current substitution
+--	    2. current goal
+--	    3. current substitution
 --   2. A stack of triplets as in the previous item
 --   3. An index of first fresh variable
 type Triplet = (P, [A], Subst)
@@ -66,7 +77,7 @@ refresh (C c ts) = Monad.liftM (C c) $ mapM refresh ts
 -- Refresh all variables in atomic formula
 refreshA :: A -> St.State (Var, Map.Map Var Var) A
 refreshA (p, ts) = Monad.liftM (p,) $ mapM refresh ts
-  
+
 -- Refresh all variables in a horn clause
 refreshH :: H -> St.State (Var, Map.Map Var Var) H
 refreshH (a :- as) = Monad.liftM (\ (a:as) -> (a :- as)) $ mapM refreshA (a:as)
@@ -86,7 +97,16 @@ eval p g = evalRec p $ initState p g
 
 -- Recursive evalutor
 evalRec :: P -> State -> [Subst] 
-evalRec p ((hs, g, subst), stack, fresh) = undefined
+evalRec _ ((_, [], subst), [], _) = [subst]
+evalRec _ (([], _, _), [], _) = []
+evalRec p ((_, [], subst), top : st, fresh) = subst : evalRec p (top, st, fresh)
+evalRec p (([], _, _), top : st, fresh) = evalRec p (top, st, fresh)
+evalRec p ((h : p', g : gs, subst), stack, fresh) = let (head' :- tail', fresh') = rename h fresh in
+                                                                      case unify (Just subst) head' g of
+                                                                        Just subst' -> case tail' of
+                                                                                        [] -> evalRec p ((p, apply subst' gs, subst' <+> subst), (p', g : gs, subst) : stack, fresh')
+                                                                                        _ -> evalRec p ((p, apply subst' (tail' ++ gs), subst' <+> subst), (p', g : gs, subst) : stack, fresh')
+                                                                        Nothing -> evalRec p ((p', g : gs, subst), stack, fresh')
 
 ------------------------------------------
 --- Some relations for natural numbers ---
@@ -96,6 +116,9 @@ evalRec p ((hs, g, subst), stack, fresh) = undefined
 x = V 0
 y = V 1
 z = V 2
+t = V 3
+u = V 4
+v = V 5
 
 --- Terms
 o   = C 0 []
@@ -108,19 +131,118 @@ lt  (x, y)    = (2, [x, y])
 le  (x, y)    = (3, [x, y])
 
 --- Specifications
-peano = [add (o, x, x) :- [], add (s(x), y, s(z)) :- [add (x, y, z)]]
+addPeano = [add (o, x, x) :- [], add (s(x), y, s(z)) :- [add (x, y, z)]]
+mulPeano = [mul (o, x, o) :- [], mul (s(x), y, z) :- [add (t, y, z), mul (x, y, t)]]
+ltPeano  = [lt (o, s(x)) :- [], lt (s(x), s(y)) :- [lt (x, y)]]
+lePeano  = [le (o, x) :- [], le (s(x), s(y)) :- [le (x, y)]]
+
+peano = addPeano ++ mulPeano ++ ltPeano ++ lePeano
+
+fromNat :: Integer -> T
+fromNat 0 = o
+fromNat n = s (fromNat $ n - 1)
+
+one = s (o)
+two = s (s (o))
+
+toNat :: T -> Integer
+toNat (C 0 []) = 0
+toNat (C 1 [t]) = 1 + toNat t
+toNat _ = undefined
+
+showPeano n = show $ toNat n
 
 --- Samples
-s0 = case eval peano [add (s(o), s(o), x)] of
+s0 = case eval peano [add (one, one, x)] of
        []    -> "error: should find a solution"
-       h : _ -> "solution: " ++ (show $ apply h x)
+       h : _ -> "solution: " ++ showPeano (apply h x)
 
-s1 = case eval peano [add (x, s(o), s (s (o)))] of
+s1 = case eval peano [add (x, one, two)] of
        []    -> "error: should find a solution"
-       h : _ -> "solution: " ++ (show $ apply h x)
+       h : _ -> "solution: " ++ showPeano (apply h x)
 
-s2 = case eval peano [add (x, y, s (s (o)))] of
+s2 = case eval peano [add (x, y, two)] of
        []               -> "error: should find a soultion"
-       h1 : h2 : h3 : _ -> "solutions: x = " ++ (show $ apply h1 x) ++ ", y = " ++ (show $ apply h1 y) ++ "\n" ++
-                           "           x = " ++ (show $ apply h2 x) ++ ", y = " ++ (show $ apply h2 y) ++ "\n" ++
-                           "           x = " ++ (show $ apply h3 x) ++ ", y = " ++ (show $ apply h3 y) ++ "\n"
+       h1 : h2 : h3 : _ -> "solutions: x = " ++ showPeano (apply h1 x) ++ ", y = " ++ showPeano (apply h1 y) ++ "\n" ++
+                           "           x = " ++ showPeano (apply h2 x) ++ ", y = " ++ showPeano (apply h2 y) ++ "\n" ++
+                           "           x = " ++ showPeano (apply h3 x) ++ ", y = " ++ showPeano (apply h3 y) ++ "\n"
+
+s3 = case eval peano [mul (two, two, x)] of
+       []    -> "error: should find a solution"
+       h : _ -> "solution: " ++ showPeano (apply h x)
+
+s4 = case eval peano [mul (x, y, fromNat 6)] of
+       h1 : h2 : h3 : h4 : _ -> "solutions: x = " ++ showPeano (apply h1 x) ++ ", y = " ++ showPeano (apply h1 y) ++ "\n" ++
+                                "           x = " ++ showPeano (apply h2 x) ++ ", y = " ++ showPeano (apply h2 y) ++ "\n" ++
+                                "           x = " ++ showPeano (apply h3 x) ++ ", y = " ++ showPeano (apply h3 y) ++ "\n" ++
+                                "           x = " ++ showPeano (apply h4 x) ++ ", y = " ++ showPeano (apply h4 y) ++ "\n"
+       _    -> "error: should find a solution"
+
+s5 = case eval peano [mul (x, two, fromNat 5)] of
+       []    -> "ok: 2 doesn't divide 5"
+       h : _ -> "error: solution: " ++ showPeano (apply h x)
+
+s6 = case eval peano [lt (one, x), lt (one, y), mul (x, y, fromNat 10)] of
+       h1 : h2 : _ -> "solutions: x = " ++ showPeano (apply h1 x) ++ ", y = " ++ showPeano (apply h1 y) ++ "\n" ++
+                                "           x = " ++ showPeano (apply h2 x) ++ ", y = " ++ showPeano (apply h2 y) ++ "\n"
+       _           -> "error: should find a solution"
+
+s7 = case eval peano [le (x, two), lt (one, x)] of
+       []    -> "error: should find a solution"
+       h : _ -> "solution: " ++ showPeano (apply h x)
+
+nil = C 2 []
+cons t l = C 3 [t, l]
+
+append (l, t, u) = (4, [l, t, u])
+minElem (x, l) = (5, [x, l])
+sorted (l, l') = (6, [l, l'])
+
+listAppend = [append (nil, x, x) :- [], append (cons x y, z, cons x t) :- [append (y, z, t)]]
+listMin    = [minElem (x, nil) :- [], minElem (x, cons y z) :- [le (x, y), minElem (x, z)]]
+listSort   = [sorted (nil, nil) :- [], sorted (x, cons y z) :- [append (t, cons y u, x),
+                                                                append (t, u, v),
+                                                                minElem (y, x),
+                                                                sorted (v, z)]]
+listBuggySort   = [sorted (nil, nil) :- [], sorted (x, cons y z) :- [append (t, cons y u, x),
+                                                                append (t, u, v),
+                                                                minElem (y, z),
+                                                                sorted (v, z)]]
+
+lists = peano ++ listAppend ++ listMin ++ listSort
+listsBuggy = peano ++ listAppend ++ listMin ++ listBuggySort
+
+toPList = foldr cons nil
+
+toHList (C 2 []) = []
+toHList (C 3 [t, l]) = t : toHList l
+toHList _ = undefined
+
+showPList l = show $ map toNat (toHList l)
+
+l0 = case eval lists [append (toPList [one], toPList [two], x)] of
+       []    -> "error: should find a solution"
+       h : _ -> "solution: " ++ showPList (apply h x)
+
+l1 = case eval lists [append (toPList [one, x], toPList [two], toPList [one, fromNat 10, two])] of
+       []    -> "error: should find a solution"
+       h : _ -> "solution: " ++ showPeano (apply h x)
+
+l2 = case eval lists [sorted (toPList [fromNat 3, fromNat 1, fromNat 5, fromNat 4], x)] of
+       []    -> "error: should find a solution"
+       h : _ -> "solution: " ++ showPList (apply h x)
+
+-- Diverges
+l2Buggy = case eval listsBuggy [sorted (toPList [fromNat 3, fromNat 1, fromNat 5, fromNat 4], x)] of
+       []    -> "error: should find a solution"
+       h : _ -> "solution: " ++ showPList (apply h x)
+
+mulBuggy = addPeano ++ [mul (o, x, o) :- [], mul (s(x), y, z) :- [mul (y, x, t), add (t, y, z)]]
+
+-- Diverges, doesn't find any solutions
+s4Buggy = case eval mulBuggy [mul (x, y, fromNat 6)] of
+       h1 : h2 : h3 : h4 : _ -> "solutions: x = " ++ showPeano (apply h1 x) ++ ", y = " ++ showPeano (apply h1 y) ++ "\n" ++
+                                "           x = " ++ showPeano (apply h2 x) ++ ", y = " ++ showPeano (apply h2 y) ++ "\n" ++
+                                "           x = " ++ showPeano (apply h3 x) ++ ", y = " ++ showPeano (apply h3 y) ++ "\n" ++
+                                "           x = " ++ showPeano (apply h4 x) ++ ", y = " ++ showPeano (apply h4 y) ++ "\n"
+       _    -> "error: should find a solution"
